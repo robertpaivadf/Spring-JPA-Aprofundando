56231f3b824221f5de7ce8a345abd14cc6a65a64
robertpaivadf <robertpaiva.df@gmail.com>
2023-03-14 22:29:28 -0300

    Início projeto Localização com Spring JPA

---
+++ git.bat
@@ -0,0 +1,7 @@
+echo "# Spring-JPA-Aprofundando" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin git@github.com:robertpaivadf/Spring-JPA-Aprofundando.git
+git push -u origin main
